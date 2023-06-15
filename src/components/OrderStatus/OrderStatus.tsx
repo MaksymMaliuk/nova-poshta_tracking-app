d114912c283c6dbe9ad403e60ae0c63b80c8a05f
Maksym <maksymmaliuk98@gmail.com>
2023-06-15 18:59:44 +0300

    static info added

--- src/components/OrderStatus/OrderStatus.tsx
+++ src/components/OrderStatus/OrderStatus.tsx
@@ -1,15 +1,29 @@
 import { Flex } from '@chakra-ui/react';
-import React from 'react';
+import React, { FC } from 'react';
 
-export const OrderStatus = () => (
-  <Flex 
-    p={4}
-    borderRadius={8}
-    boxShadow='0 0 4px #0000001f'
-    minW='400px' 
-    minH='400px' 
-    background='#fff'
-  >
-    Order Status
-  </Flex>
-);
+export const OrderStatus: FC = () => {
+  return  (
+    <Flex 
+      p='40px 24px'
+      borderRadius={8}
+      flexDir='column'
+      gap={4}
+      boxShadow='0 0 4px #0000001f'
+      minW='400px' 
+      height='auto' 
+      background='#fff'
+    >
+      <div>Статус доставки: Одержано</div>
+
+      <div>
+        <b>Відправлено:</b>
+        <p>Відділення №17 (до 30кг): вул. Розумовська, 29</p>
+      </div>
+
+      <div>
+        <b>Отримано:</b>
+        <p>Відділення №3 (до 30кг): просп. Гагаріна, 43</p>
+      </div>
+    </Flex>
+  );
+};
