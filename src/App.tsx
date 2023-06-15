b7189e06441cbbb1434596ea298414f3c4b72ad1
Maksym <maksymmaliuk98@gmail.com>
2023-06-15 18:10:45 +0300

    pre commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Flex } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { OrderStatus } from './components/OrderStatus/OrderStatus';
+import { OrderStatus } from './components/OrderStatus';
 
 export const App: FC = () => (
   <Flex direction="column" height='100vh'>
