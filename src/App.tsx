463c8b01b914b228f59df370ebdb72f5ec729795
Maksym <maksymmaliuk98@gmail.com>
2023-06-19 22:12:49 +0300

    add: orders history in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import { Flex } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { OrderStatus } from './components/OrderStatus';
 import { FormComponent } from './components/FormComponent';
 import { Response } from './types/types';
 import { sendPostRequest } from './api/requests';
+import { useLocalStorage } from './custom-hooks/useLocalStorage';
 
 export const App: React.FC = () => {
   const [order, setOrder] = useState<Response | null>(null);
 
+  const documentNumber = order?.data.find(item => item.Number);
+  
+  const [storedOrders, setStoredOrders] = useLocalStorage<Response[]>('orders', []);
+  console.log(storedOrders);
+  
+  const addOrder = (item: Response | null) => {
+    const orderExist = storedOrders.some((orders: Response) => (
+      orders.data.map(orderData => orderData.Number === documentNumber?.Number)
+    ));
+
+    if (!orderExist) {
+      const updatedOrders = [...storedOrders, item];
+      setStoredOrders(updatedOrders);
+    }
+  };
+
+  useEffect(() => {
+    const storedOrder = localStorage.getItem('phones');
+
+    if (order) {
+      addOrder(order); 
+      return;
+    }
+    
+    if (storedOrder) {
+      setStoredOrders(JSON.parse(storedOrder));
+    }
+  }, [order]);
+
   const handleSendStatusRequest = async (tnnNumber: string): Promise<void> => {
     const response = await sendPostRequest(tnnNumber);
     setOrder(response);
-    console.log(response);
   };
 
   return (
