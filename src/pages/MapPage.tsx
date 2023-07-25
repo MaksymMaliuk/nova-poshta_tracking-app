d5bb0273c5a358b3a1d673be45e70008be53fa46
Maksym <maksymmaliuk98@gmail.com>
2023-07-25 10:39:19 +0300

    fix

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapComponent } from '../components/MapComponent';
 import { Flex } from '@chakra-ui/layout';
+import { Input } from '@chakra-ui/input';
 
 const center = {
   lat: 48.745736308404624,
@@ -11,10 +12,14 @@ export const MapPage: React.FC = () => (
   <Flex 
     flex='1' 
     alignItems='center' 
-    background='#fafafa' 
-    justifyContent='center' 
+    background='#fafafa'
+    flexDir='column'
+    justifyContent='center'
     gap={10}
   >
+    <Input
+      width='400px'
+    />
     <MapComponent center={center} />
   </Flex>
 );
